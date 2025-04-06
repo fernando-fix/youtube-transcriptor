a17ae8c450521f4d026a34a03a6a52d96c31cb67
Fernando Rodrigues <ferando_tecti@hotmail.com>
2025-04-06 10:47:09 -0300

    Melhoria em obtenção de legendas

--- api.py
+++ api.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api._errors import TranscriptsDisabled, NoTranscriptFound
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/transcription', methods=['GET'])
 def get_transcription():
@@ -10,23 +13,27 @@ def get_transcription():
         return jsonify({'error': 'Parâmetro video_id é obrigatório.'}), 400
 
     try:
-        # Obtém a transcrição em português brasileiro
-        transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['pt-BR'])
-        
+        try:
+            transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['pt'])
+        except NoTranscriptFound:
+            transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['pt-BR'])
+
         # Verifica se o parâmetro "with_times" foi fornecido
         with_times = request.args.get('with_times')
-        
+
         if with_times is None:
-            # Se não informar "with_times", retorna a transcrição concatenada sem os tempos
             full_text = " ".join(entry['text'] for entry in transcript)
             return jsonify({'transcription': full_text})
         else:
-            # Se informar "with_times", retorna a transcrição com os tempos
             full_text_with_times = " ".join(f"[{entry['start']:.2f}s] {entry['text']}" for entry in transcript)
             return jsonify({'transcription': full_text_with_times})
+
+    except TranscriptsDisabled:
+        return jsonify({'error': 'As transcrições estão desativadas para este vídeo.'}), 403
+    except NoTranscriptFound:
+        return jsonify({'error': 'Nenhuma transcrição disponível para esse vídeo.'}), 404
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    # A API ficará acessível em todas as interfaces na porta 5000
     app.run(host='0.0.0.0', port=5000)
